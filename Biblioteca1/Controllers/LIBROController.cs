f764b86c3ffa9909af017b0f5a841e088e3fd3cc
Jonathan <jonathan.1701510578@ucaldas.edu.co>
2021-02-28 13:32:08 -0500

    primer commit

---
+++ Biblioteca1/Controllers/LIBROController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Biblioteca1.Models;
+
+namespace Biblioteca1.Controllers
+{
+    public class LIBROController : Controller
+    {
+        private GESTION_BIBLIOTECAEntities db = new GESTION_BIBLIOTECAEntities();
+
+        // GET: LIBRO
+        public ActionResult Index()
+        {
+            return View(db.LIBRO.ToList());
+        }
+
+        // GET: LIBRO/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LIBRO lIBRO = db.LIBRO.Find(id);
+            if (lIBRO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lIBRO);
+        }
+
+        // GET: LIBRO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LIBRO/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NOMBRE,AUTOR,GENERO")] LIBRO lIBRO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LIBRO.Add(lIBRO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lIBRO);
+        }
+
+        // GET: LIBRO/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LIBRO lIBRO = db.LIBRO.Find(id);
+            if (lIBRO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lIBRO);
+        }
+
+        // POST: LIBRO/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NOMBRE,AUTOR,GENERO")] LIBRO lIBRO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lIBRO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lIBRO);
+        }
+
+        // GET: LIBRO/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LIBRO lIBRO = db.LIBRO.Find(id);
+            if (lIBRO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lIBRO);
+        }
+
+        // POST: LIBRO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LIBRO lIBRO = db.LIBRO.Find(id);
+            db.LIBRO.Remove(lIBRO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Biblioteca1/Controllers/USUARIOController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Biblioteca1.Models;
+
+namespace Biblioteca1.Controllers
+{
+    public class USUARIOController : Controller
+    {
+        private GESTION_BIBLIOTECAEntities db = new GESTION_BIBLIOTECAEntities();
+
+        // GET: USUARIO
+        public ActionResult Index()
+        {
+            return View(db.USUARIO.ToList());
+        }
+
+        // GET: USUARIO/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // GET: USUARIO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: USUARIO/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NOMBRE,APELLIDO,CC,CONTRASEÑA")] USUARIO uSUARIO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIO.Add(uSUARIO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uSUARIO);
+        }
+
+        // GET: USUARIO/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // POST: USUARIO/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NOMBRE,APELLIDO,CC,CONTRASEÑA")] USUARIO uSUARIO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uSUARIO);
+        }
+
+        // GET: USUARIO/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // POST: USUARIO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            db.USUARIO.Remove(uSUARIO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
